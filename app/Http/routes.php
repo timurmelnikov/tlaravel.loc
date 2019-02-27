8f4e1cbe902872f0a8312b3763a910cb447a4126
Timur Melnikov <melnikovt@gmail.com>
2019-02-27 17:19:10 +0200

    Методы регистрации маршрутов. Часть 2

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,9 +11,13 @@
 |
 */
 
-Route::get('/', function () {
+Route::get('/', ['as' => 'home', function () {
     return view('welcome');
-});
+}]);
+
+Route::get('/article/{id}', ['as' => 'article', function ($id) {
+    echo $id;
+}]);
 
 
 Route::get('/page/{cat}/{id}/', function ($var1, $var2) {
@@ -27,3 +31,48 @@ Route::get('/page/{cat}/{id}/', function ($var1, $var2) {
 //    echo 'rrrr';
 //    echo '</pre>';
 //});
+
+
+//Route::get('/page/{cat}/{id}/', function ($var1, $var2) {
+//    echo '<pre>';
+//
+//    echo $var1 . '|' . $var2;
+//    echo '</pre>';
+//
+//})->where(['cat'=>'[A-Za-z]+']);
+
+
+//Route::post('/comments', function (){
+//
+//   print_r($_POST);
+//});
+
+//Route::match(['get', 'post'],'/comments', function (){
+//
+//   print_r($_POST);
+//});
+
+//Route::any('/comments', function (){
+//    print_r($_POST);
+//});
+
+
+Route::group(['prefix' => 'admin/{id}'], function () {
+    Route::get('/page/create/{v}', function ($v) {
+        //echo route('home');
+        //return redirect()->route('home');
+        //return redirect()->route('article', ['id'=>25]);
+
+        $routes = Route::current();
+        //echo $routes->getName();
+        //echo $routes->getParameter('v', 111);
+        print_r($routes->parameters());
+
+    })->name('createpage');
+
+
+    Route::get('/page/edit/', function () {
+        echo 'page/edit';
+    });
+
+});
